e7ff9908f3b4774b20af7c49622c047ea16137d5
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-02-09 16:37:15 +0600

    Initial Commit

---
+++ Navigation_New/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation_New
+//
+//  Created by Анастасия Здобнова on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray
+    }
+
+
+}
+
