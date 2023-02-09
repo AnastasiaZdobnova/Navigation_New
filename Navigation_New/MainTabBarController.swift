97848d838c4b3e3c969a745e861a45b9d4bf3b6e
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-02-09 20:25:56 +0600

    add NC

--- Navigation_New/MainTabBarController.swift
+++ Navigation_New/MainTabBarController.swift
@@ -15,27 +15,46 @@ class MainTabBarController: UITabBarController {
         setTabBarAppearance()
     }
     
+// VC
+//    private func generateTabBar(){
+//        viewControllers = [
+//            generateVC(viewController: FeedViewController(), title: "Feed", image: UIImage(systemName: "house.fill")),
+//            generateVC(viewController: ProfileViewController(), title: "Profile", image : UIImage(systemName: "person.fill"))
+//        ]
+//    }
+//
+//    private func generateVC(viewController: UIViewController, title: //VC String, image: UIImage?) ->UIViewController {
+//        viewController.tabBarItem.title = title
+//        viewController.tabBarItem.image = image
+//        return viewController
+//    }
+    
+    //NC
     private func generateTabBar(){
-        viewControllers = [
-            generateVC(viewController: FeedViewController(), title: "Feed", image: UIImage(systemName: "house.fill")),
-            generateVC(viewController: ProfileViewController(), title: "Profile", image : UIImage(systemName: "person.fill"))
-        ]
+            viewControllers = [
+                generateNC(viewController: FeedViewController(), itemName: "Feed", itemImage: UIImage(systemName: "house.fill")),
+                generateNC(viewController: ProfileViewController(), itemName: "Profile_new", itemImage : UIImage(systemName: "person.fill"))
+            ]
     }
-    
-    private func generateVC(viewController: UIViewController, title: String, image: UIImage?) ->UIViewController {
-        viewController.tabBarItem.title = title
-        viewController.tabBarItem.image = image
-        return viewController
-    }
-    
+        //NC
+    private func generateNC(viewController: UIViewController, itemName: String, itemImage: UIImage?) ->UINavigationController {
+        let item = UITabBarItem(
+            title: itemName,
+            image: itemImage,
+            tag: 0)
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem = item
+        return navController
+       }
+
     private func setTabBarAppearance(){
-        
+
         let positionOnX: CGFloat = 10
         let positionOnY: CGFloat = 14
         let width = tabBar.bounds.width - positionOnX * 2
         let height = tabBar.bounds.height + positionOnY * 2
         let roundLayer = CAShapeLayer()
-        
+
         let bezierPath = UIBezierPath(
             roundedRect: CGRect(x: positionOnX, y: tabBar.bounds.minY - positionOnY, width: width, height: height),
             cornerRadius: height/2)
@@ -43,12 +62,14 @@ class MainTabBarController: UITabBarController {
         tabBar.layer.insertSublayer(roundLayer, at: 0)
         tabBar.itemWidth = width/5
         tabBar.itemPositioning = .centered
-        
+
         roundLayer.fillColor = UIColor.mainWhite.cgColor
         tabBar.tintColor = .tabBarItemAccent
         tabBar.unselectedItemTintColor = .tabBarItemLight
-        
-    }
 
+    }
+    
+    
+    
 }
 
