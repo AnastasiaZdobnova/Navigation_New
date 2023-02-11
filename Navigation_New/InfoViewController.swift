4c01d6b2bf7aea35332ee27a199a757a6452c3cd
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2023-02-11 15:28:59 +0600

    add alert

--- Navigation_New/InfoViewController.swift
+++ Navigation_New/InfoViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemYellow
-       
+        myButton()
+        
+        
+        
     }
     
+    @objc func showAlert() {
+        let alertController = UIAlertController(title: "Alert Title", message: "Alert Message", preferredStyle: .alert)
+        let firstAction = UIAlertAction(title: "First Action", style: .default) { (action) in
+            print("First Action pressed")
+        }
+        let secondAction = UIAlertAction(title: "Second Action", style: .default) { (action) in
+            print("Second Action pressed")
+        }
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    func myButton(){
+        let button = UIButton(frame: CGRect(x: 100, y: 150, width: 200,  height: 70))
+        button.layer.cornerRadius = 10
+        view.addSubview(button)
+        button.setTitle("Show Alert", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .mainWhite
+        button.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+    }
     
-
+        
+    
+    
+    
 }
